35c304ae2ea59e7bf099487e1a28c5909df74b87
taofeek-gmind <gmindconcept@gmail.com>
2026-09-06 18:06:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { AuthProvider, useAuth } from './context/AuthContext'
+import ... from "./components/AuthContext";
 import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
